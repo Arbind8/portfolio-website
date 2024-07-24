120cc947216ac16c507851a14f88db5d085a2e74
Arbind Das <arbinddas95@gmail.com>
2024-07-24 23:05:27 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+var typed = new Typed (".text", {
+
+strings:["Frontend Developer","Android Developer","Web Developer"],
+typeSpeed:120,
+backSpeed:80,
+delaySpeed:1000,
+loop: true,
+
+});
+
+var typed = new Typed (".name",{
+
+
+})
